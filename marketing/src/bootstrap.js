711e8115649050fabe12f9fe38fa23a83c643fbd
yescine <bouchoucha.yassine@gmail.com>
2022-09-09 20:42:17 +0100

    fix webpack and add React

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,1 +1,14 @@
-console.log("Marketing")
+import React from 'react'
+import ReactDom from 'react-dom'
+
+export const mount = (el) => {
+  ReactDom.render(
+    <h1>Marketing</h1>,
+    el
+  )
+}
+const isDev = process.env.NODE_ENV === "development"
+if (isDev) {
+  const devRoot = document.querySelector("#_marketing-dev-root")
+  if (devRoot) mount(devRoot)
+}
--- marketing/webpack/webpack.common.js
+++ marketing/webpack/webpack.common.js
@@ -2,12 +2,12 @@ module.exports = {
   module: {
     rules: [
       {
-        test: "/\.m?js$/",
+        test: /\.m?js$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ["@babel/react-env", "@babel/preset-env"],
+            presets: ["@babel/preset-react", "@babel/preset-env"],
             plugins: ["@babel/plugin-transform-runtime"],
           }
         }
