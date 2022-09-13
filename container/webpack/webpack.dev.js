4989f015e0509b3515fe8f06af2dcca4aa82f323
yescine <bouchoucha.yassine@gmail.com>
2022-09-13 12:23:03 +0100

    share all dependancies

--- container/webpack/webpack.dev.js
+++ container/webpack/webpack.dev.js
@@ -2,6 +2,7 @@ const { merge } = require('webpack-merge')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
 const commonConf = require('./webpack.common')
+const {dependencies} = require('../package.json')
 
 const devConfig = {
   mode: "development",
@@ -18,10 +19,9 @@ const devConfig = {
       remotes: {
         marketing: "marketing@http://localhost:8081/remoteEntry.js"
       },
-      shared: [
-        "react",
-        "react-dom"
-      ]
+      shared: {
+        ...dependencies
+      }
 
     }),
     new HtmlWebpackPlugin({
--- marketing/webpack/webpack.dev.js
+++ marketing/webpack/webpack.dev.js
@@ -2,6 +2,7 @@ const { merge } = require('webpack-merge')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const commonConf = require('./webpack.common')
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
+const {dependencies} = require('../package.json')
 
 const devConfig = {
   mode: "development",
@@ -18,10 +19,9 @@ const devConfig = {
       exposes: {
         './MarketingApp': "./src/bootstrap"
       },
-      shared: [
-        "react",
-        "react-dom"
-      ]
+      shared: {
+        ...dependencies
+      }
 
     }),
     new HtmlWebpackPlugin({
