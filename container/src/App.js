4923a19b44ee50b5c50a197b1480067974eeb824
yescine <bouchoucha.yassine@gmail.com>
2022-09-16 09:20:55 +0100

    add header and adjsut prod public path

--- container/src/App.js
+++ container/src/App.js
@@ -2,15 +2,17 @@ import React from 'react'
 import { Switch, Route, BrowserRouter } from 'react-router-dom'
 import { StylesProvider } from '@material-ui/core/styles'
 import MarketingApp from './components/MarketingApp'
+import Header from './components/Header'
 
 export default () => {
 
   return (
+    <BrowserRouter>
+      <div>
+        <Header />
+        <MarketingApp />
+      </div>
+    </BrowserRouter>
 
-    <div>
-      <h1>Welcome</h1>
-      <hr />
-      <MarketingApp />
-    </div>
   )
 }
